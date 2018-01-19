d529a2f876b598b51c7768f7396497034babb02f
Jay Schwane <jrschwane@uwalumni.com>
2018-01-19 15:19:00 -0600

    Adds KnexJS database config files.

---
+++ db/knex.js
@@ -0,0 +1,9 @@
+/*
+    Purpose: setup KnexJS with environment-specific configuration
+*/
+
+const environment = process.env.NODE_ENV || 'development';
+const config      = require('../knexfile')[environment];
+const knex        = require('knex');
+
+module.exports = knex(config);
---
+++ knexfile.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+module.exports = {
+    development: {
+        client: 'pg',
+        connection: {
+            host     : '127.0.0.1',
+            user     : process.env.DATABASE_USER,
+            password : process.env.DATABASE_PASSWORD,
+            database : process.env.DATABASE_NAME
+        },
+        migrations: {
+            directory: './db/migrations'
+        },
+        seeds: {
+            directory: './db/seeds'
+        }
+    },
+    production: {
+        client     : 'pg',
+        connection : process.env.DATABASE_URL,
+        migrations : {
+            directory: './db/migrations'
+        },
+        seeds: {
+            directory: './db/seeds/production'
+        }
+    }
+};
