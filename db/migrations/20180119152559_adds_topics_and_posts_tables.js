f5c0420eb4c645db2e2ccca04967543918841c38
Jay Schwane <jrschwane@uwalumni.com>
2018-01-20 17:03:12 -0600

    Adds topics_posts join table to Knex migration file

--- db/migrations/20180119152559_adds_topics_and_posts_tables.js
+++ db/migrations/20180119152559_adds_topics_and_posts_tables.js
@@ -1,19 +1,27 @@
 exports.up = function(knex, Promise) {
     return Promise.all([
         knex.schema.createTable('topics', (table) => {
-            table.increments();
+            table.increments('id').primary();
             table.string('topic').notNullable();
             table.timestamp('created_at').defaultTo(knex.fn.now());
             table.timestamp('updated_at').defaultTo(knex.fn.now());
         }),
 
         knex.schema.createTable('posts', (table) => {
-            table.increments();
+            table.increments('id').primary();
             table.string('title').notNullable();
             table.text('body').notNullable();
             table.integer('views').defaultTo(0);
             table.integer('likes').defaultTo(0);
-            table.specificType('topics', 'jsonb[]');
+            table.timestamp('created_at').defaultTo(knex.fn.now());
+            table.timestamp('updated_at').defaultTo(knex.fn.now());
+        }),
+
+        // create join table for many-to-many between posts and topics
+        knex.schema.createTable('topics_posts', (table) => {
+            table.increments('id').primary();
+            table.integer('post_id').references('id').inTable('posts');
+            table.integer('topic_id').references('id').inTable('topics');
             table.timestamp('created_at').defaultTo(knex.fn.now());
             table.timestamp('updated_at').defaultTo(knex.fn.now());
         })
@@ -25,6 +33,7 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
     return Promise.all([
+        knex.schema.dropTable('topics_posts'),
         knex.schema.dropTable('posts'),
         knex.schema.dropTable('topics')
     ]);
