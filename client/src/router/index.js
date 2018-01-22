c125845a30ca9fdcf6c76ceef9399039f552a6a0
Jay Schwane <jrschwane@uwalumni.com>
2018-01-22 17:12:39 -0600

    Adds routes to Posts and Compose components

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue     from 'vue';
+import Router  from 'vue-router';
+import Posts   from '@/components/Posts';
+import Compose from '@/components/Compose';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+    routes: [
+        {
+            path      : '/',
+            name      : 'Posts',
+            component : Posts
+        },
+        {
+            path      : '/compose',
+            name      : 'Compose',
+            component : Compose
+        }
+    ]
+});
